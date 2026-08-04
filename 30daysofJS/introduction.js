8319587ef4c64b8aa0cd480c00b982664bd86be2
Zharick <zharickfh12@gmail.com>
2026-08-03 22:40:18 -0500

    day1

---
+++ 30daysofJS/introduction.js
@@ -0,0 +1,28 @@
+/* datos primitivos de JavaScript: Cadenas de texto ('string'), Números, Booleanos, undefined, Null y Symbol
+    numeros: integers y float
+    cadenas de texto: string
+    booleanos: true o false
+    undefined: variable declarada pero no inicializada
+    null: variable declarada e inicializada con valor nulo
+*/
+
+// typeof operador devuelve el tipo de dato de una variable
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // número
+console.log(typeof true ) // boolean
+console.log(typeof null) // tipo de objeto
+console.log(typeof undefined) // undefined
+
+/* Declaración de variables: var, let y const
+    var: variable global o local en una función sin importar el bloque de código
+    let: variable local en un bloque de código
+    const: variable local en un bloque de código, no puede ser reasignada
+*/
+
+// Declarando diferentes variables de diferentes tipos de datos
+let nombre = 'Asabeneh'
+let apellido = 'Yetayeh'
+let edad = 100
+let estaCasado = true
+
+console.log(nombre, apellido, pais, ciudad, edad, estaCasado)
